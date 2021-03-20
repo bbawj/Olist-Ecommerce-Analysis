685b751aca3212e816aedca7dd4d71b6fee04d30
unknown <brendanawjang@gmail.com>
2021-03-21 00:13:06 +0800

    translated category names

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ data3 = pd.read_csv("olist_order_payments_dataset.csv")
 data4 = pd.read_csv("olist_order_reviews_dataset.csv")
 data5 = pd.read_csv("olist_orders_dataset.csv")
 data6 = pd.read_csv("olist_products_dataset.csv")
-
+data7 = pd.read_csv("product_category_name_translation.csv")
 
 new = pd.merge(data2, data3, on='order_id')
 new1 = pd.merge(new, data4, on='order_id')
 new2 = pd.merge(new1, data5, on='order_id')
 new3 = pd.merge(new2, data6, on='product_id')
 print(len(data2.order_id.unique()))
+
+# translate category names to english
+translate = data7.set_index('product_category_name').to_dict()[
+    'product_category_name_english']
+
+new3['product_category_name'].replace(translate, inplace=True)
+
+print(new3["product_category_name"])
