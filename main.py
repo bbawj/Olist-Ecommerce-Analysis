f701c87d173c74e335b4f334383ba6b8466de4c9
Justyn <justynphoa@gmail.com>
2021-03-22 00:23:10 +0800

    jupyter

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
+# Basic Libraries
+import numpy as np
 import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt # we only need pyplot
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.tree import plot_tree
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import export_graphviz
+import graphviz
+from IPython.display import display
+sb.set() # set the default Seaborn style for graphics
 
-#data1 = pd.read_csv("olist_customers_dataset.csv")
-data2 = pd.read_csv("olist_order_items_dataset.csv")
-data3 = pd.read_csv("olist_order_payments_dataset.csv")
-data4 = pd.read_csv("olist_order_reviews_dataset.csv")
-data5 = pd.read_csv("olist_orders_dataset.csv")
-data6 = pd.read_csv("olist_products_dataset.csv")
-data7 = pd.read_csv("product_category_name_translation.csv")
 
-new = pd.merge(data2, data3, on='order_id')
-new1 = pd.merge(new, data4, on='order_id')
-new2 = pd.merge(new1, data5, on='order_id')
-new3 = pd.merge(new2, data6, on='product_id')
-print(len(data2.order_id.unique()))
 
-# translate category names to english
-translate = data7.set_index('product_category_name').to_dict()[
-    'product_category_name_english']
 
-new3['product_category_name'].replace(translate, inplace=True)
-
-print(new3["product_category_name"])
